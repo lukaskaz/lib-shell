a2d4c5bd0e22a1256fc0069bc22c784bd633fec1
Lukasz Kazmierczak <lukask84@o2.pl>
2024-07-04 12:10:44 +0200

    Code formatting update
    Signed-off-by: Lukasz Kazmierczak <lukask84@o2.pl>

--- examples/basic/src/main.cpp
+++ examples/basic/src/main.cpp
@@ -3,12 +3,15 @@
 #include <iostream>
 #include <memory>
 
-int main() {
-  try {
-    auto cmdshandler = std::make_shared<BashCommand>();
-  } catch (std::exception &err) {
-    std::cerr << "[ERROR] " << err.what() << '\n';
-  }
-
-  return 0;
+int main()
+{
+    try
+    {
+        auto cmdshandler = std::make_shared<BashCommand>();
+    }
+    catch (std::exception& err)
+    {
+        std::cerr << "[ERROR] " << err.what() << '\n';
+    }
+    return 0;
 }
