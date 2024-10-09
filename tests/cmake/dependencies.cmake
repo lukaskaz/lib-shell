7e4f547b8d2130e735c3cdf2ff8b629db2ecc287
lukaskaz <lukask84@o2.pl>
2024-10-09 04:01:49 +0200

    Removing boost dependencies (#3)
    Signed-off-by: Lukasz Kazmierczak <lukask84@o2.pl>

--- tests/cmake/dependencies.cmake
+++ tests/cmake/dependencies.cmake
@@ -1,8 +1,6 @@
 cmake_minimum_required(VERSION 3.10)
 
-find_package(Boost 1.74.0 REQUIRED COMPONENTS filesystem)
-# find_package(nlohmann_json 3.8.1 REQUIRED)
-# find_package(CURL REQUIRED)
+find_package(Boost 1.74.0 COMPONENTS filesystem)
 
 include(ExternalProject)
 
@@ -25,3 +23,30 @@ EXTERNALPROJECT_ADD(
 include_directories(${source_dir}/googletest/include)
 include_directories(${source_dir}/googlemock/include)
 link_directories(${build_dir}/lib)
+
+IF(NOT Boost_FOUND)
+
+include(ExternalProject)
+set(source_dir "${CMAKE_BINARY_DIR}/libboost-src")
+set(build_dir "${CMAKE_BINARY_DIR}/libboost-build")
+
+EXTERNALPROJECT_ADD(
+  libboost
+  URL               "https://boostorg.jfrog.io/artifactory/main/release/1.74.0/source/boost_1_74_0.tar.gz"
+  PATCH_COMMAND     ""
+  PREFIX            libboost-workspace
+  SOURCE_DIR        ${source_dir}
+  BINARY_DIR        ${build_dir}
+  CONFIGURE_COMMAND cd ${source_dir} && ./bootstrap.sh --prefix=${build_dir} 
+    --with-libraries=filesystem --without-icu
+  BUILD_COMMAND     cd ${source_dir} && ./b2 install -j8 --disable-icu 
+    --ignore-site-config "cxxflags=-fPIC" threading=multi link=shared
+  UPDATE_COMMAND    ""
+  INSTALL_COMMAND   "" 
+  TEST_COMMAND      ""
+)
+
+include_directories(${build_dir}/include)
+link_directories(${build_dir}/lib)
+
+ENDIF()
